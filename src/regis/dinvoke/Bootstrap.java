b6a4e062484248b24d90a825984f0f18e65744a4
Regis <xu.regis@gmail.com>
2011-07-28 23:41:28 +0800

    InvokeDynamic annotation support to specific bootstrap method now.

---
+++ src/regis/dinvoke/Bootstrap.java
@@ -0,0 +1,16 @@
+package regis.dinvoke;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Bootstrap {
+	String className() default "regis.dinvoke.BootstrapUtils";
+
+	String methodName() default "bootstrap";
+
+	String methodType() default "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;[Ljava/lang/Object;)Ljava/lang/invoke/CallSite;";
+}
---
+++ src/regis/dinvoke/BootstrapUtils.java
@@ -0,0 +1,43 @@
+package regis.dinvoke;
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.ConstantCallSite;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+
+import org.objectweb.asm.Opcodes;
+
+public class BootstrapUtils {
+
+	public static CallSite bootstrap(Lookup lookup, String name,
+			MethodType methodType, Object... bsmArgs) {
+
+		try {
+			Class owner = Class.forName((String) bsmArgs[0], true, lookup
+					.getClass().getClassLoader());
+			int opcode = ((Integer) bsmArgs[1]).intValue();
+
+			MethodHandle handler = null;
+			switch (opcode) {
+			case Opcodes.INVOKEINTERFACE:
+			case Opcodes.INVOKEVIRTUAL:
+				handler = lookup.findVirtual(owner, name, methodType);
+				break;
+			case Opcodes.INVOKESPECIAL:
+				handler = lookup.findSpecial(owner, name, methodType, owner);
+				break;
+			case Opcodes.INVOKESTATIC:
+				handler = lookup.findStatic(owner, name, methodType);
+				break;
+			}
+			return new ConstantCallSite(handler);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
